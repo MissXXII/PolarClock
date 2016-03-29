16907eeef9b384a9115291fe270f64575139d571
Mickaël <missvingtdeux@gmail.com>
2016-03-29 15:58:19 +0200

    Add Date Arc
    - Adding the arc for the date
    - Adding a function that determinate how many days of the current month
    : useful to know if if the arc is divided in 31 or 30 or less for
    february. To make it works, I had to get current month and current
    year. I had an init function to initialize all current times.

--- src/model.js
+++ src/model.js
@@ -1,10 +1,14 @@
 function Model() {
     EventEmitter.call(this); // Call EventEmitter constructor
     // Model Initialisation
-    this._milliseconds = this.setMillisecond();
-    this._seconds = this.setSecond();
-    this._minutes = this.setMinute();
-    this._hours = this.setHour();
+    this._milliseconds = 0;
+    this._seconds = 0;
+    this._minutes = 0;
+    this._hours = 0;
+    this._date = 0;
+    this._day = 0;
+    this._month = 0;
+    this._year = 0;
 }
 
 // EventEmitter inheritance
@@ -13,40 +17,72 @@ Model.prototype.constructor = Model;
 
 // Start the clock
 Model.prototype.start = function() {
+    this.init();
     this._tic();
-    window.requestAnimationFrame(this.start.bind(this));
+};
+
+// Init
+Model.prototype.init = function() {
+    this._setMillisecond();
+    this._setSecond();
+    this._setMinute();
+    this._setHour();
+    this._setDate();
+    this._setDay();
+    this._setMonth();
+    this._setYear();
 };
 
 // Tic...
 Model.prototype._tic = function() {
     this.emit('tic');
-    this.setMillisecond();
-    this.setSecond();
-    this.setMinute();
-    this.setHour();
+    this._setMillisecond();
+    this._setSecond();
+    this._setMinute();
+    this._setHour();
+    this._setDate();
+    this._setDay();
+    this._setMonth();
+    window.requestAnimationFrame(this._tic.bind(this));
 };
 
 
-//// GET TIME IN ITS OWN VALLUE ////////////////////////////////////////
-Model.prototype.getHour = function() {
-    return this._hours;
+//// GET TIME IN ITS OWN VALLUE ////////////////////////////////////////////////////////////////////
+Model.prototype._getMillisecond = function() {
+    return this._milliseconds;
 };
 
-Model.prototype.getMinute = function() {
-    return this._minutes;
-};
-
-Model.prototype.getSecond = function() {
+Model.prototype._getSecond = function() {
     return this._seconds;
 };
 
-Model.prototype.getMillisecond = function() {
-    return this._milliseconds;
+Model.prototype._getMinute = function() {
+    return this._minutes;
 };
-///////////////////////////////////////////////////////////////////////
+
+Model.prototype._getHour = function() {
+    return this._hours;
+};
+
+Model.prototype._getDate = function() {
+    return this._date;
+};
+
+Model.prototype._getDay = function() {
+    return this._day;
+};
+
+Model.prototype._getMonth = function() {
+    return this._month;
+};
+
+Model.prototype._getYear = function() {
+    return this._year;
+};
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-//// GET TIME IN OSER VALLUES ////////////////////////////////////////
+//// GET TIME IN OSER VALLUES //////////////////////////////////////////////////////////////////////
 Model.prototype.getSecondInMls = function() {
     var actualS = this._seconds;
     var actualMls = this._milliseconds;
@@ -75,63 +111,115 @@ Model.prototype.getHourInS = function() {
     return hInS;
 };
 
-Model.prototype.getHourInS = function() {
-    var mInS = this.getMinuteInS();
+Model.prototype.getHourInMin = function(){
+    var actualMin = this._minutes;
     var actualH = this._hours;
-    var hInS = (actualH  * 60 * 60) + mInS;
-    return hInS;
+    var hInMin = (actualH * 60) + actualMin;
+    return hInMin;
 };
 
+Model.prototype.getDateInMin = function() {
+    var hInMin = this.getHourInMin();
+    var actualDate = this._date;
+    // a day has 24 hours so if we are the 1st the day is actually passing from 0 to 24h
+    var dateInMin = ((actualDate - 1) * 24 * 60) + hInMin;
+    return dateInMin;
+};
 
-///////////////////////////////////////////////////////////////////////
+Model.prototype.getDayInMin = function() {
+    var hInMin = this.getHourInMin();
+    var actualDay = this._day;
+    // a day has 24 hours so if we are the 1st the day is actually passing from 0 to 24h
+    var dayInMin = ((actualDay > 0 ? actualDay -= 1 : jour = 6) * 24 * 60) + hInMin;
+    return dayInMin;
+};
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-//// GET TIME IN DEGREE ////////////////////////////////////////
+//// GET TIME IN DEGREE ////////////////////////////////////////////////////////////////////////////
 Model.prototype.getSecondInDegree = function () {
     var actualS = this.getSecondInMls();
-    var secondInDegree = actualS * (360 / 60000);
+    var secondInDegree = actualS * (360 / (60 * 1000));
     return secondInDegree;
 };
 
 Model.prototype.getMinuteInDegree = function () {
     var actualM = this.getMinuteInMls();
-    var minuteInDegree = actualM * (360 / 3600000);
+    var minuteInDegree = actualM * (360 / (60 * 60 * 1000));
     return minuteInDegree;
 };
 
 Model.prototype.getHourInDegree = function () {
     var actualH = this.getHourInS();
-    var hourInDegree = actualH * (360 / 86400);
+    var hourInDegree = actualH * (360 / (24 * 60 * 60));
     return hourInDegree;
 };
-
-///////////////////////////////////////////////////////////////////////
-
-
-
-
-//// SETTING TIME ///////////////////////////////////////////////////////////////////
-Model.prototype.setHour = function(hour) {
-    var newDate = new Date();
-    this._hours = newDate.getHours();
-    this.emit('hChange');
+// Get how many days in the actual month
+Model.prototype.getDaysMaxInMonth = function (year, month) {
+    // when Date is created, give it parameters to get the date you want
+    // Here we need to know how many days there are in the current month
+    // So we give to new Date the current year,
+    // the current month+1 = next month, with the 3rd parameter that is 0 for day 0
+    // For example : (2016 , 2 + 1 , 0) => 2016 3(April) 0 = 2016 2(March) 31(last day)
+    return new Date(year, month + 1, 0).getDate();
 };
 
-Model.prototype.setMinute = function() {
+Model.prototype.getDateInDegree = function () {
+    var actualDate = this.getDateInMin();
+    // How many days in the actual month
+    var daysMax = this.getDaysMaxInMonth(this._getYear(),this._getMonth());
+    var dateInDegree = actualDate * (360 / (daysMax * 24 * 60));
+    return dateInDegree;
+};
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//// SETTING TIME //////////////////////////////////////////////////////////////////////////////////
+Model.prototype._setMillisecond = function() {
     var newDate = new Date();
-    this._minutes = newDate.getMinutes();
-    this.emit('mChange');
+    this._milliseconds = newDate.getMilliseconds();
+    this.emit('msChange');
 };
 
-Model.prototype.setSecond = function() {
+Model.prototype._setSecond = function() {
     var newDate = new Date();
     this._seconds = newDate.getSeconds();
     this.emit('sChange');
 };
 
-Model.prototype.setMillisecond = function() {
+Model.prototype._setMinute = function() {
     var newDate = new Date();
-    this._milliseconds = newDate.getMilliseconds();
-    this.emit('msChange');
+    this._minutes = newDate.getMinutes();
+    this.emit('mChange');
 };
-///////////////////////////////////////////////////////////////////////
+
+Model.prototype._setHour = function() {
+    var newDate = new Date();
+    this._hours = newDate.getHours();
+    this.emit('hChange');
+};
+
+Model.prototype._setDate = function() {
+    var newDate = new Date();
+    this._date = newDate.getDate();
+    this.emit('dateChange');
+};
+
+Model.prototype._setDay = function() {
+    var newDate = new Date();
+    this._day = newDate.getDay();
+    this.emit('dayChange');
+};
+
+Model.prototype._setMonth = function() {
+    var newDate = new Date();
+    this._month = newDate.getMonth();
+    this.emit('monthChange');
+};
+
+Model.prototype._setYear = function() {
+    var newDate = new Date();
+    this._year = newDate.getFullYear();
+    this.emit('yearChange');
+};
+////////////////////////////////////////////////////////////////////////////////////////////////////
--- src/view.js
+++ src/view.js
@@ -31,11 +31,12 @@ View.prototype._bindActions = function() {
     this._model.on('sChange', this._drawSecond.bind(this));
     this._model.on('mChange', this._drawMinute.bind(this));
     this._model.on('hChange', this._drawHour.bind(this));
-    
+
     this._model.on('tic', this.canvasRefresh.bind(this));
-    this._model.on('sChange', this._drawSecondInArc.bind(this));
-    this._model.on('mChange', this._drawMinuteInArc.bind(this));
-    this._model.on('hChange', this._drawHourInArc.bind(this));
+    this._model.on('sChange', this._drawSecondArc.bind(this));
+    this._model.on('mChange', this._drawMinuteArc.bind(this));
+    this._model.on('hChange', this._drawHourArc.bind(this));
+    this._model.on('dateChange', this._drawDateArc.bind(this));
 };
 
 // Redraw the clock
@@ -45,16 +46,18 @@ View.prototype._redraw = function() {
     this._drawMinute();
     this._drawHour();
 
-    this._secondArc = new Arc(240, 25, 'orange', this.ctx);
-    this._minuteArc = new Arc(210, 25, 'blue', this.ctx);
-    this._hourArc = new Arc(180,25,'green', this.ctx);
+    this._secondArc = new Arc(240, 25, 'red', this.ctx);
+    this._minuteArc = new Arc(210, 25, 'orange', this.ctx);
+    this._hourArc = new Arc(180, 25, 'gold', this.ctx);
+    this._dateArc = new Arc(150, 25, 'purple', this.ctx);
     this.canvasRefresh();
-    this._drawSecondInArc();
-    this._drawMinuteInArc();
-    this._drawHourInArc();
+    this._drawSecondArc();
+    this._drawMinuteArc();
+    this._drawHourArc();
+    this._drawDateArc();
 };
 
-//// DRAW TIME AS ARC ////////////////////////////////////////
+//// DRAW TIME AS ARC //////////////////////////////////////////////////////////////////////////////
 View.prototype.canvasRefresh = function() {
     this.ctx.setTransform(1, 0, 0, 1, 0, 0);
     this.ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -62,25 +65,29 @@ View.prototype.canvasRefresh = function() {
     this.ctx.rotate(-90 * Math.PI / 180);
 };
 
-
-View.prototype._drawSecondInArc = function() {
+View.prototype._drawSecondArc = function() {
     var secondInDegree = this._model.getSecondInDegree();
     this._secondArc.drawArc(secondInDegree);
 };
 
-View.prototype._drawMinuteInArc = function() {
+View.prototype._drawMinuteArc = function() {
     var minuteInDegree = this._model.getMinuteInDegree();
     this._minuteArc.drawArc(minuteInDegree);
 };
 
-View.prototype._drawHourInArc = function() {
+View.prototype._drawHourArc = function() {
     var hourInDegree = this._model.getHourInDegree();
     this._hourArc.drawArc(hourInDegree);
 };
-///////////////////////////////////////////////////////////////////////
+
+View.prototype._drawDateArc = function() {
+    var dateInDegree = this._model.getDateInDegree();
+    this._dateArc.drawArc(dateInDegree);
+};
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-//// DRAW TIME AS DIV ////////////////////////////////////////
+//// DRAW TIME AS DIV //////////////////////////////////////////////////////////////////////////////
 
 // how to draw the div
 View.prototype._drawDiv = function(divWidth, color, time) {
@@ -94,8 +101,8 @@ View.prototype._drawSecond = function() {
     var second = this._model.getSecondInMls();
     $('#drawS').empty();
     var divWidth;
-    divWidth = second * (100 / 60000);
-    var div = this._drawDiv(divWidth, 'orange', (Math.floor(second / 1000)) + ' / 60 secondes');
+    divWidth = second * (100 / (60 * 1000));
+    var div = this._drawDiv(divWidth, 'red', (this._model._getSecond()) + ' s');
     $('#drawS').append(div);
 };
 
@@ -103,8 +110,8 @@ View.prototype._drawMinute = function() {
     var minute = this._model.getMinuteInMls();
     $('#drawM').empty();
     var divWidth;
-    divWidth = minute * (100 / 3600000);
-    var div = this._drawDiv(divWidth, 'blue', (Math.floor(minute / 60000)) + ' / 60 minutes');
+    divWidth = minute * (100 / (60 * 60 * 1000));
+    var div = this._drawDiv(divWidth, 'orange', (this._model._getMinute()) + ' min');
     $('#drawM').append(div);
 };
 
@@ -112,17 +119,25 @@ View.prototype._drawHour = function() {
     var hour = this._model.getHourInS();
     $('#drawH').empty();
     var divWidth;
-    divWidth = hour * (100 / 86400);
-    var div = this._drawDiv(divWidth, 'green', (Math.floor(hour / 3600)) + ' / 24 heures');
+    divWidth = hour * (100 / (24 * 60 * 60 ));
+    var div = this._drawDiv(divWidth, 'gold', (this._model._getHour()) + ' h');
     $('#drawH').append(div);
 };
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+//// DRAW TIME AS TEXT /////////////////////////////////////////////////////////////////////////////
 
-// Set hour to model value
-View.prototype._drawHourInTxt = function() {
-    var hour = this._model.getHour();
-    this._hour.innerHTML = hour < 10 ? '0' + hour : hour;
+// Set millissecond to model value
+View.prototype._drawMillisecondInTxt = function() {
+    var millisecond = this._model.getMillisecond();
+    this._millisecond.innerHTML = millisecond;
+};
+
+// Set second to model value
+View.prototype._drawSecondInTxt = function() {
+    var second = this._model.getSecond();
+    this._second.innerHTML = second;
 };
 
 // Set minute to model value
@@ -131,16 +146,9 @@ View.prototype._drawMinuteInTxt = function() {
     this._minute.innerHTML = minute < 10 ? '0' + minute : minute;
 };
 
-
-// Set millissecond to model value
-View.prototype._drawSecondInTxt = function() {
-    var second = this._model.getSecond();
-    this._second.innerHTML = second;
+// Set hour to model value
+View.prototype._drawHourInTxt = function() {
+    var hour = this._model.getHour();
+    this._hour.innerHTML = hour < 10 ? '0' + hour : hour;
 };
-
-
-// Set millissecond to model value
-View.prototype._drawMillisecondInTxt = function() {
-    var millisecond = this._model.getMillisecond();
-    this._millisecond.innerHTML = millisecond;
-};
+////////////////////////////////////////////////////////////////////////////////////////////////////
